c78077000773bff28e1d3e8f023a1f72c795796f
Wesley <wesley.sequeira@hotmail.com>
2020-04-07 15:01:15 -0700

    simple solution

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Hamiltonion-Path
 Print all Hamiltonion Paths
+
+To compile: 
+
+```
+clang++ -std=c++11 -stdlib=libc++ -o main main.cpp
+```
+
+To run:
+
+```
+./main
+```
--- main.cpp
+++ main.cpp
@@ -3,6 +3,9 @@
 
 using namespace std;
 
+int START;
+int END;
+
 struct Edge {
 	int src, dest;
 };
@@ -27,6 +30,44 @@ public:
 	}
 };
 
+void printAllHamiltonianPaths(Graph const& g, int x, vector<bool>
+						visited, vector<int> &path, int N)
+{
+	// if all the vertices are visited, then
+	// Hamiltonian path exists
+	if (path.size() == N && *path.begin() == START && path[N-1] == END)
+	{
+
+		// print Hamiltonian path
+		for (int i : path)
+			cout << i << " ";
+		cout << endl;
+
+		return;
+	}
+
+	// Check if every edge starting from vertex v leads
+	// to a solution or not
+	for (int w : g.adj_list[x])
+	{
+		// process only unvisited vertices as Hamiltonian
+		// path visits each vertex exactly once
+		if (!visited[w])
+		{
+			visited[w] = true;
+			path.push_back(w);
+
+			// check if adding vertex w to the path leads
+			// to solution or not
+			printAllHamiltonianPaths(g, w, visited, path, N);
+
+			// Backtrack
+			visited[w] = false;
+			path.pop_back();
+		}
+	}
+}
+
 // Represent a random graph with n vertices of at least degree one (n is at least 100). 
 // This graph might be undirected or directed. It could have cycles or no cycles. 
 // Randomly select two vertices (x,y) from the graph, and find all of the possible hamiltonian paths in the graph.
@@ -35,14 +76,30 @@ int main() {
 	vector<Edge> edges = {
 		{0, 1}, {1, 2}, {1, 3}, {2, 3}
 	};
+	vector<int> vertices = {0,1,2,3};
     cout<<"Choose from the following vertices:\n";
     cout<<"0,1,2,3\n";
 
     int start,end;
+
     cout<<"enter starting vertex x:" << endl;
     cin>>start;
-    cout<<"enter ending vertex y:" << endl;
+	while(true){
+		if(find(vertices.begin(), vertices.end(), start)==vertices.end()){
+			cout<<"Choose from the following existing vertices:" << endl;
+			cin>>start;
+		} else {break;}
+	}
+
+	cout<<"enter ending vertex y:" << endl;
     cin>>end;
+	while(true){
+		if(find(vertices.begin(), vertices.end(), end)==vertices.end()){
+			cout<<"Choose from the following existing vertices:" << endl;
+			cin>>end;
+		} else {break;}
+	}
+    
     cout<<"x: " << start << " y: " << end << endl;
 
 	int N = 4;
@@ -57,6 +114,10 @@ int main() {
 	vector<bool> visited(N);
 	visited[start] = true;
 
-	return 0;
+	START = start;
+	END = end;
 
+	printAllHamiltonianPaths(g, start, visited, path, N);
+
+	return 0;
 }
